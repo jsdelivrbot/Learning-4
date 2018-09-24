23706e0c63807928c494f4ac45a385afd9dad5c8
ville <ville.virta@adrianaonline.com>
2018-09-24 15:32:14 +0200

    deleted submodules

---
+++ React/BookList/src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function() {
+    return [
+        {title: 'Javascript: The good Parts', pages: 101},
+        {title: 'Another Book', pages: 123},
+        {title: 'Yet another Bookssie Book',pages: 12},
+    ];
+}
